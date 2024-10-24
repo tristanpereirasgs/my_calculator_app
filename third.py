a3a0023a4a39fd8dd281aae8a3df9d152d14bfd9
tristanpereirasgs <tristanpereirasgs@gmail.com>
2024-10-24 13:35:57 +0600

    Add files via upload

---
+++ third.py
@@ -0,0 +1,14 @@
+#Input numbers
+a= int(input("please enter your number"))
+b= int(input("please enter another number"))
+
+#Addition
+print (f'the sum is', (a+b))
+#Subraction
+print (f'the subtraction is', (a-b))
+#Multiplication
+print (f'the multiplication is', (a*b))
+#Division
+print (f'the division is', (a/b))
+#Percentage
+print (f'the percentage is',(a%b)) 
